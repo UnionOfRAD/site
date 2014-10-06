7cdf1c3d5ed4a55e20d466147b4f83dfa0bf5f9e
David Persson <davidpersson@gmx.de>
2014-10-06 20:04:57 +0200

    Adding error pages.

--- app/config/bootstrap/errors.php
+++ app/config/bootstrap/errors.php
@@ -21,7 +21,7 @@ ErrorHandler::apply('lithium\action\Dispatcher::run', array(), function($info, $
 	Media::render($response, compact('info', 'params'), array(
 		'library' => true,
 		'controller' => '_errors',
-		'template' => 'fiveohoh',
+		'template' => $info['exception']->getCode() == 404? 'fourohfour' : 'fiveohoh',
 		'layout' => 'default',
 		'request' => $params['request']
 	));
--- app/views/_errors/fiveohoh.html.php
+++ app/views/_errors/fiveohoh.html.php
@@ -1,5 +1,6 @@
 <article class="error error-500">
-
-	<h1 class="h-alpha t-red">Internal Server Error</h1>
-
+	<h1 class="h-alpha t-red">500 Internal Server Error</h1>
+	<p>
+		Something bad happened.
+	</p>
 </article>
