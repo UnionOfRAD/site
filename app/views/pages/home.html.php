adb2e5a8d2a354df8ddba66ceefc28ed97a3995c
David Persson <davidpersson@gmx.de>
2015-08-13 21:41:48 +0200

    Fix link.

--- app/views/pages/home.html.php
+++ app/views/pages/home.html.php
@@ -23,7 +23,7 @@ $this->set([
 				<?= $this->html->link('Quickstart', ['library' => 'li3_docs', 'action' => 'view', 'controller' => 'ApiBrowser', 'lib' => 'manual', 'args' => ['quickstart']], ['class' => 'button large quickstart']) ?>
 				<?= $this->html->link('Source', 'https://github.com/unionofrad/lithium', ['class' => 'button icon large source']) ?>
 			</nav>
-			<a href="https://github.com/UnionOfRAD/lithium/tree/1.0-rc1" target="new" class="download button large">
+			<a href="https://github.com/UnionOfRAD/lithium/tree/v1.0.0-rc1" target="new" class="download button large">
 				<span class="download__title">Download</span>
 				<span class="download__version">
 					li₃ version 1.0.0-rc1
